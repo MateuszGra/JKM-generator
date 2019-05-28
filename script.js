3d81a49e95d022f3259f28d26bf66c15e19d6f3d
MateuszGra <mxgrabowski@gmail.com>
2019-05-28 16:14:24 +0200

    styled

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 (() => {
     const generate = document.querySelector('.generate');
     const button = document.querySelector('button');
-    const butonImg = document.querySelector('button img');
+    const butonImg = document.querySelector('.imgButton');
     const text = [
         ["Ja chcę powiedzieć jedną rzecz, ", "Trzba powiedzieć jasno, ", "Jak powiedział wybitny krakowianin Stanisław Lem, ", "Proszę mnie dobrze zrozumieć ", "Ja chciałem przypomnieć Państwu, że ", "Niech nie mają Państwo złudzeń, ", "Powiedzmy to wyraźnie, "],
+
         ["przedstawiciele czerwonej hołoty ", "ci wszyscy (tfu!) geje ", "funkcjonriusze reżymowej telewizji ", "tak zwani ekolodzy ", "ci wszyscy (tfu!) demokraci ", "agenci bezpieki ", "feminaziski "],
+
         ["zupełnie bezkarnie ", "całkowicie bezczelnie ", "o poglądach na lewo od komunizmu ", "celowo i świadomie ", "z premedytacją ", "od czasów Okrągłego Stołu ", "w ramach postępu ", ],
+
         ["nawołują do podniesienia podatków ", "próbują wyrzucić kierowców z miast ", "próbują skłócić Polskę z Rosją ", "głoszą brednie o globalnym ociepleniu ", "zakazują posiadnia broni ", "nie dopuszczają prawicy do władzy ", "uczą dzieci homoseksualizmu "],
+
         ["bo dzięku temu mogą kraść ", "bo dostają za to pieniądze ", "bo tak się uczy w państwowej szkole ", "bo bez tego (tfu!) demokracja nie może istnieć ", "bo głupich jest więcej niż mądrych ", "bo chcą tworzyć raj na ziemi ", "bo chcą niszczyć cywilizację białego człowieka ", ],
+
         ["przez kolejne kadnecje.", "o czym się nie mówi.", "i własnie dlatego umiera Europa.", ", ale przyjdą muzłumanie i zrobią porządek.", " - tak samo zresztą jak za hitlera. ", " - proszę zobaczyć co się dzieje na Zachodzie, jeśli mi Państwo nie wierzą.", "co lat temu sto nie przyszło by nawet do głowy."]
     ]
 
